efeb6121308ddb8eff58c6b672e6960aa3af1494
Tyler Wallace <tywallace2017@gmail.com>
2020-09-08 17:41:19 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -53,7 +53,7 @@ describe "smallest hash value" do
   end
 
   it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
+    key_for_min_value = ({:blake => 10, :ashley => 50, :adam => 17})
     p key_for_min_value[0]
   end
 
